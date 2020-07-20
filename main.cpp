1adae52107ab58893be40967c228c25a7b540979
YeonSang <samshin3910@snu.ac.kr>
2020-07-20 19:31:31 +0900

    Baekjoon 2164

--- main.cpp
+++ main.cpp
@@ -1,59 +1,36 @@
 #include <iostream>
-#include <string>
 using namespace std;
-#define size 20000001
+#define size 5000001
 
 int* arr = new int[size];
-int last = 0;
 int start = 0;
+int last = 0;
 
-void switch_string(string s){
-    if(s == "push"){
-        int num;
-        cin >> num;
-        arr[last] = num;
-        last++;
-    }
+void pop() {
+    start++;
+}
 
-    else if(s == "pop") {
-        if(last - start == 0) cout << -1 << '\n';
-        else {
-            cout << arr[start] << '\n';
-            start++;
-        }
-    }
-
-    else if(s == "size"){
-        cout << last - start << '\n';
-    }
-
-    else if(s == "empty"){
-        if(last - start == 0) cout << 1 << '\n';
-        else cout << 0 << '\n';
-    }
-
-    else if(s == "front"){
-        if(last - start == 0) cout << -1 << '\n';
-        else cout << arr[start] << '\n';
-    }
-
-    else if(s == "back"){
-        if(last - start == 0) cout << -1 << '\n';
-        else cout << arr[last - 1] << '\n';
-    }
+void pop_and_push_back(){
+    arr[last] = arr[start];
+    last++;
+    start++;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
     int N;
     cin >> N;
-    while(N--){
-        string s;
-        cin >> s;
-        switch_string(s);
+    //initialization
+    last = N;
+    for(int i = 0; i < N; i++) {
+        arr[i] = i+1;
     }
 
-    delete[] arr;
+    //ordering until one number lasts
+    while(last - start != 1) {
+        pop();
+        pop_and_push_back();
+    }
+
+    cout << arr[start] << '\n';
+
 }
