eb9b011b89cb9ee0297fa88769182c314517469b
YeonSang <samshin3910@snu.ac.kr>
2020-07-28 17:31:36 +0900

    Baekjoon 6549

--- main.cpp
+++ main.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
-#include <vector>
+#include <stdio.h>
+#include <stack>
 using namespace std;
 
-long long int multiply_split(long long int a, long long int b, long long int c) {  // a^b = ? (mod c)
-    if(b == 0) return 1;
-    if(b%2 == 1) return ((a%c) * (multiply_split(a, b/2, c)) % c * (multiply_split(a, b/2, c) % c)) % c;
-    else return ((multiply_split(a, b/2, c) % c) * (multiply_split(a, b/2, c) % c)) % c;
-}
-
+int n;
+long long int arr[100002];
 int main() {
-    long long int N, K;
-    cin >> N >> K;
-    long long int DIVIDER = 1000000007;
-    vector<long long int> index_factorial;
-    index_factorial.push_back(1);  // 0! = 1
-    long long int val = 1;
+    while(true) {
+        scanf("%d",&n);
+        // ending program
+        if(n == 0) break;
 
-    for(long long int i = 1; i <= N; i++){
-        val = (val * i) % DIVIDER;
-        index_factorial.push_back(val);
+        // initialization
+        for(int i = 0; i < 100002; i++){
+            arr[i] = 0;
+        }
+
+        // input histogram
+        for(int i = 0; i < n; i++) {
+            scanf("%d",&arr[i]);
+        }
+
+        // solution using <stack>
+        stack<int> s;
+        long long int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            // first step : fill the stack until the last one
+            while(!s.empty() && arr[s.top()] > arr[i]) {
+                long long int height = arr[s.top()];
+                s.pop();
+                int width = i;
+                if (!s.empty()) {
+                    width = (i - s.top() - 1);
+                }
+                if (ans < width*height) {
+                    ans = width*height;
+                }
+            }
+            s.push(i);
+        }
+        // second step : when first step finished but the stack is not empty
+        while(!s.empty()) {
+            long long int height = arr[s.top()];
+            s.pop();
+            long long int width = n;
+            if (!s.empty()) {
+                width = n-s.top()-1;
+            }
+            if (ans < width*height) {
+                ans = width*height;
+            }
+        }
+        printf("%lld\n",ans);
     }
-    long long int temp1 = (index_factorial[K] * index_factorial[N-K]) % DIVIDER;
-    long long int temp2 = multiply_split(temp1, DIVIDER-2, DIVIDER) % DIVIDER;
-    long long int temp3 = (index_factorial[N] * temp2) % DIVIDER;
-    cout << temp3 << '\n';
 }
