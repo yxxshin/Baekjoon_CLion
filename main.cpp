bd344cb24aea19f4840651d0d803beb75610f08d
YeonSang <samshin3910@snu.ac.kr>
2020-07-29 21:48:38 +0900

    Baekjoon 1920

--- main.cpp
+++ main.cpp
@@ -1,77 +1,52 @@
 #include <cstdio>
-#include <vector>
 #include <algorithm>
-
 using namespace std;
 
-struct Point {
-    int x, y;
-};
+int N, M;
 
-int distance(Point& p, Point& q) {
-    return (p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y);
-}
-
-bool compare_x(Point& p, Point& q){
-    return (p.x < q.x);
-}
-
-bool compare_y(Point& p, Point& q){
-    return (p.y < q.y);
-}
-
-int findMin(vector<Point>::iterator it, int n){
-    // when n=2 or n=3
-    if(n==2) return distance(it[0], it[1]);
-    if(n==3) return min( {distance(it[0], it[1]), distance(it[1],it[2]), distance(it[2],it[0])});
-
-    // else
-    // set the middle line and divide-and-conquer
-    // 3 cases: left side / right side / including the line
-    int line = (it[n/2 - 1].x + it[n/2].x) / 2;     // middle line
-    int minDistance = min(findMin(it, n/2), findMin(it + n/2, n - n/2));   // left side & right side
-
-    // including the line
-    vector<Point> middle;
-    middle.reserve(n);
-
-    // push_back points which distances from line is lower than minDistance
-    // only consider these points
-    for(int i = 0; i < n; i++){
-        int temp = line -it[i].x;
-        if( temp * temp < minDistance) middle.push_back(it[i]);
-    }
-
-    // sort by y-value
-    sort(middle.begin(), middle.end(), compare_y);
-
-    // keypoint: if the difference of y-value gets larger than minDistance, you don't need to consider the left points
-    int size = middle.size();
-    for(int i = 0; i < size - 1; i++){
-        for(int j = i + 1; j < size && (middle[j].y - middle[i].y) * (middle[j].y - middle[i].y) < minDistance; j++) {
-            minDistance = min(minDistance, distance(middle[i], middle[j]));
+void Binaray_Search(int* arr, int target) {
+    // arr should be sorted
+    int left = 0, right = N - 1;
+    while(left <= right) {
+        int middle = (left + right) / 2;
+        if(arr[middle] > target) {
+            right = middle - 1;
+            continue;
+        }
+        else if(arr[middle] < target) {
+            left = middle + 1;
+            continue;
+        }
+        else if(arr[middle] == target) {
+            printf("1\n");
+            return;
         }
     }
-
-    return minDistance;
-
+    // target doesn't exist in arr
+    printf("0\n");
 }
 
-
 int main() {
-    // input n
-    int n;
-    scanf("%d",&n);
-
-    // make Point vector and get input
-    vector<Point> vec(n);
-    for(vector<Point>::iterator it = vec.begin(); it != vec.end(); it++) {
-        scanf("%d %d", &it->x, &it->y);
+    // get inputs
+    scanf("%d",&N);
+    int* arr = new int[N];
+    for(int i = 0; i < N; i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&M);
+    int* brr = new int[M];
+    for(int i = 0; i < M; i++){
+        scanf("%d",&brr[i]);
     }
 
-    // sort by x-value
-    sort(vec.begin(), vec.end(), compare_x);
+    // sort array
+    sort(arr, arr+N);
 
-    // print
-    printf("%d\n", findMin(vec.begin(), n));
+    // Binaray Search
+    for(int i = 0; i < M; i++){
+        Binaray_Search(arr, brr[i]);
+    }
+
+    delete[] arr;
+    delete[] brr;
 }
