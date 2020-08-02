a89114f06df59ffd13f0fd1c82e185c7a8fb424e
YeonSang <samshin3910@snu.ac.kr>
2020-08-03 01:53:47 +0900

    Baekjoon 2293

--- main.cpp
+++ main.cpp
@@ -1,42 +1,29 @@
 #include <cstdio>
-#include <queue>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    int N;
-    // make two priority queues: max and min
-    // max_pq is less<int>, min_pq is greater<int>
-    // every numbers in min_pq are bigger than every numbers in max_pq
-    // every time, max_pq.size() should be same or 1 bigger than min_pq.size()
-    priority_queue<int, vector<int>, less<int>> max_pq;
-    priority_queue<int, vector<int>, greater<int>> min_pq;
-
-    scanf("%d",&N);
-    while(N--) {
-        // get input
-        int num;
-        scanf("%d", &num);
-
-        // push num
-        if(max_pq.size() == min_pq.size()) {
-            max_pq.push(num);
-        }
-        else min_pq.push(num);
-
-        // sort
-        if(!min_pq.empty() && max_pq.top() > min_pq.top()) {
-            // swap the two top values
-            int min_temp = min_pq.top();
-            int max_temp = max_pq.top();
-            min_pq.pop();
-            max_pq.pop();
-            min_pq.push(max_temp);
-            max_pq.push(min_temp);
-        }
-
-        // print middle value: top of max_pq
-        printf("%d\n", max_pq.top());
+    int n, k;
+    scanf("%d %d",&n,&k);
+    int* coins = new int[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d",&coins[i]);
     }
 
+    // Solution by Dynamic Programming
+    int dp[10001] = {0,};
+    dp[0] = 1;  // initialization
+
+    // overwrite array dp while increasing coin types
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= k; j++) {
+            if(coins[i] <= j)   // use dp[0]=1
+                dp[j] += dp[j-coins[i]];
+        }
+    }
+
+
+    printf("%d\n", dp[k]);
+    delete[] coins;
 }
