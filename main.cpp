a00c364986532a405d81033fc2949d194a1c084c
YeonSang <samshin3910@snu.ac.kr>
2020-08-09 19:16:33 +0900

    Baekjoon 10942

--- main.cpp
+++ main.cpp
@@ -3,52 +3,88 @@
 
 using namespace std;
 
-int N, M;
-int memory[100], cost[100];
-int dp[10001];     // dp[i] : the max value of memory using cost i
+int arr[2002];
+// dp[S][E]: check if arr[S] to arr[E] is pelindrome
+// dp = -1 : not visited, 0 : not pelindrome, 1 : pelindrome
+int dp[2002][2002];
 
-int main() {
-    // get inputs
-    scanf("%d %d",&N,&M);
-    for(int i = 0; i < N; i++){
-        scanf("%d",&memory[i]);
-    }
-    for(int i = 0; i < N; i++){
-        scanf("%d",&cost[i]);
-    }
 
-    // make dp[i] all -1, to know if visited or not
-    memset(dp, -1, sizeof(dp));
+void func(int S, int E){
+    int s = S, e = E;
+    while(s <= e && s >= S && e <= E){
+        // one number : pelindrome
+        if( s == e ) {
+            dp[s][e] = 1;
+            s--;
+            e++;
+            continue;
+        }
 
-    for(int i = 0; i < N; i++){
-        // consider about using c[i]
-        // update from the back : prevent overlapping
-        int temp_cost = cost[i];
-
-        for(int j = 10000; j >= cost[i]; j--) {
-            if(dp[j - temp_cost] == -1) {
-                // not visited
+        // adjacent number : should be same
+        else if( e - s == 1 ){
+            if(arr[s] == arr[e]) {
+                dp[s][e] = 1;
+                s--;
+                e++;
                 continue;
             }
-
-            if(dp[j - temp_cost] + memory[i] > dp[j]) {
-                // better condition : swap
-                dp[j] = dp[j -temp_cost] + memory[i];
+            else {
+                dp[s][e] = 0;
+                s--;
+                e++;
+                continue;
             }
         }
 
-        if(dp[temp_cost] < memory[i]) {
-            // when just using itself(i'th) is better : swap
-            dp[temp_cost] = memory[i];
+        // if we already visited dp[s][e], break
+        else if(dp[s][e] != -1) break;
+
+        // if we don't know dp[s+1][e-1] either
+        else if(dp[s+1][e-1] == -1){
+            s++;
+            e--;
+            continue;
+        }
+
+        // if arr[S+1] to arr[E-1] is pelindrome and arr[S]==arr[E], arr[S] to arr[E] is pelindrome
+        else if( dp[s+1][e-1] == 1 && arr[s] == arr[e]) {
+            dp[s][e] = 1;
+            s--;
+            e++;
+            continue;
+        }
+
+        // else: not pelindrome
+        else {
+            dp[s][e] = 0;
+            s--;
+            e++;
+            continue;
         }
     }
 
-    // print answer: the smallest dp[i] which is bigger than M is the answer
-    for(int i = 0; i < 10001; i++){
-        if(dp[i] >= M) {
-            printf("%d\n", i);
-            break;
-        }
-    }
+    // print
+    if(dp[S][E] == 1)
+        printf("1\n");
+    else if(dp[S][E] == 0)
+        printf("0\n");
+}
 
+int main() {
+    // input
+    int N, M;
+    scanf("%d",&N);
+    for(int i = 1; i <= N; i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&M);
+
+    // solution
+    memset(dp, -1, sizeof(dp)); // original value of dp is all -1
+
+    while(M--){
+        int S, E;
+        scanf("%d %d", &S, &E);
+        func(S, E);
+    }
 }
