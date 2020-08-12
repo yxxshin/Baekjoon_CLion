219d085614565999fd3363e6b69d8bbb7fffa017
YeonSang <samshin3910@snu.ac.kr>
2020-08-12 17:36:56 +0900

    Baekjoon 2178

--- main.cpp
+++ main.cpp
@@ -1,59 +1,64 @@
 #include <cstdio>
 #include <cstring>
+#include <queue>
 using namespace std;
 
-int map[52][52];
-int visit[52][52];
-int T, M, N, K, cnt = 0;
+int map[102][102];
+int visit[102][102];
+// to check 4 directions by 'for' loop
+int direction[4][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+int N, M;
+typedef struct Coord {
+    int x;
+    int y;
+    int dist;
+};
+queue<Coord> q;
 
-void dfs(int x, int y){
-    // visited map[x][y]
-    visit[x][y] = 1;
+void bfs(){
+    // use queue
+    // start from (0,0)
+    visit[0][0] = 1;
+    q.push( Coord{0,0,1} );
 
-    // find next node
-    if(visit[x+1][y] != 1 && map[x+1][y] == 1 && x+1 < M) {
-        dfs(x+1, y);
+    // bfs until arrival
+    while(!q.empty()){
+        // visit front node of q
+        Coord temp = q.front();
+
+        // if arrived final destination
+        if(temp.x == M-1 && temp.y == N-1){
+            printf("%d\n", temp.dist);
+            break;
+        }
+        q.pop();
+
+        // check 4 directions
+        for(int i = 0; i < 4; i++){
+            int next_x = temp.x + direction[i][0];
+            int next_y = temp.y + direction[i][1];
+            if(map[next_x][next_y] == 1 && next_x >= 0 && next_x < M && next_y >= 0 && next_y < N && visit[next_x][next_y] != 1) {
+                // if valid, push node in queue and check 'visited'
+                visit[next_x][next_y] = 1;
+                q.push( Coord{next_x, next_y, temp.dist + 1} );
+            }
+        }
     }
-
-    if(visit[x][y+1] != 1 && map[x][y+1] == 1 && y+1 < N) {
-        dfs(x, y+1);
-    }
-
-    if(visit[x-1][y] != 1 && map[x-1][y] == 1 && x-1 >= 0) {
-        dfs(x-1, y);
-    }
-
-    if(visit[x][y-1] != 1 && map[x][y-1] == 1 && y-1 >= 0) {
-        dfs(x, y-1);
-    }
-    // when no deeper node exist, return (recursion)
 }
 
 int main() {
+    // put inputs
+    scanf("%d %d", &N, &M);
 
-    scanf("%d", &T);
-    while (T--) {
-        // initialization
-        scanf("%d %d %d", &M, &N, &K);
-        cnt = 0;
-        memset(map, 0, sizeof(map));
-        memset(visit, 0, sizeof(visit));
-
-        while (K--) {
-            int input1, input2;
-            scanf("%d %d", &input1, &input2);
-            map[input1][input2] = 1;
+    // initialization
+    memset(visit, 0, sizeof(visit));
+    memset(map, 0, sizeof(map));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            scanf("%1d", &map[j][i]);
         }
-
-        // dfs
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && visit[i][j] != 1) {
-                    dfs(i, j);
-                    cnt++;
-                }
-            }
-        }
-        printf("%d\n", cnt);
     }
+
+    // bfs (solution)
+    bfs();
 }
