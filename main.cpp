babe1b4b62780eaaec1cca6de331fa8084af8eae
YeonSang <samshin3910@snu.ac.kr>
2020-08-12 18:42:35 +0900

    Baekjoon 1697

--- main.cpp
+++ main.cpp
@@ -1,79 +1,59 @@
 #include <cstdio>
 #include <cstring>
 #include <queue>
-#include <climits>
+#define MAX 100000
 using namespace std;
 
-int map[1002][1002];
-// to check 4 directions by 'for' loop
-int direction[4][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-int N, M, ans = INT_MIN;
-typedef struct Coord {
-    int i;
-    int j;
-    int dist;
-};
-queue<Coord> q;
+int visit[MAX+2];
+int N, K;
+queue< pair<int, int> > q;     // <location,time>
+
+bool isValid(int val){
+    if(val >= 0 && val <= MAX && visit[val] != 1)
+        return true;
+    else return false;
+}
 
 void bfs(){
     // use queue
     // bfs until arrival
     while(!q.empty()){
         // visit front node of q
-        Coord temp = q.front();
+        pair<int, int> temp = q.front();
         q.pop();
 
-        // check 4 directions
-        for(int i = 0; i < 4; i++){
-            int next_i = temp.i + direction[i][0];
-            int next_j = temp.j + direction[i][1];
-            if(map[next_i][next_j] == 0 && next_i >= 0 && next_i < N && next_j >= 0 && next_j < M) {
-                // if valid, push node in queue and change value of map
-                map[next_i][next_j] = 1;
-                q.push( Coord{next_i, next_j, temp.dist + 1} );
-                ans = max(ans, temp.dist + 1);
-            }
+        // solved problem
+        if(temp.first == K) {
+            // found kid
+            printf("%d\n", temp.second);
+            break;
+        }
+
+        // check all locations which can be arrived from temp in 1 sec
+        if(isValid(temp.first - 1)){
+            visit[temp.first - 1] = 1;
+            q.push(make_pair(temp.first - 1, temp.second + 1));
+        }
+
+        if(isValid(temp.first + 1)){
+            visit[temp.first + 1] = 1;
+            q.push(make_pair(temp.first + 1, temp.second + 1));
+        }
+
+        if(isValid(temp.first * 2)) {
+            visit[temp.first * 2] = 1;
+            q.push(make_pair(temp.first * 2, temp.second + 1));
         }
     }
 }
 
 int main() {
     // put inputs
-    scanf("%d %d", &M, &N);
+    scanf("%d %d", &N, &K);
 
     // initialization
-    memset(map, 0, sizeof(map));
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            scanf("%d", &map[i][j]);
-        }
-    }
-
-    // bfs (solution)
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            if(map[i][j] == 1)
-                q.push( Coord{i, j, 0} );
-                ans = 0;
-        }
-    }
-
+    memset(visit, 0, sizeof(visit));
+    visit[N] = 1;
+    q.push( make_pair(N,0) );
     bfs();
-
-    // check if problem is solvable
-    bool isPossible = true;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            if(map[i][j] == 0){
-                // cannot be solved
-                isPossible = false;
-                break;
-            }
-        }
-    }
-
-    // print answer
-    if(isPossible) printf("%d\n", ans);
-    else printf("-1\n");
 }
