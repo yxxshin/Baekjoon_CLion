3bfcf4898c4b4587db6d673de1181fa752360ac8
YeonSang <samshin3910@snu.ac.kr>
2020-08-17 00:38:47 +0900

    Baekjoon 1504

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <cstdio>
 #include <queue>
-#include <vector>
 #include <algorithm>
-#include <cstring>
 using namespace std;
 #define INF 999999999
 
-int V, E, K;
-int dist[20002];     // dist[i] : minimum distance going to node i, gets updated
-vector< pair<int,int> > map[20002];     // connected node information : {to,value}
+int N, E, v1, v2;
+int dist[802];     // dist[i] : minimum distance going to node i, gets updated
+int map[802][802];  // map[i][j] = map[j][i] : distance from node i to j, if no road 0
 
-void Dijkstra() {
+int Dijkstra(int from, int to) {
     // Dijkstra Algorithm using priority_queue
+    // initialization
+    for(int i = 1; i <= N; i++)
+        dist[i] = INF;
+
+    // definition
     priority_queue< pair<int,int> > pq;
-    pq.push( make_pair(0,K) );
-    dist[K] = 0;
+    pq.push( make_pair(0,from) );
+    dist[from] = 0;
 
     while(!pq.empty()) {
         int current_node = pq.top().second;
@@ -22,39 +25,45 @@ void Dijkstra() {
         pq.pop();
 
         // update values of dist
-        for(int i = 0; i < map[current_node].size(); i++){
-            int test_node = map[current_node][i].first;
-            int new_cost = cost + map[current_node][i].second;
-            int before_cost = dist[test_node];
+        for(int i = 1; i <= N; i++){
+            // test every node connected with 'current_node'
+            if(map[current_node][i] == 0) continue;
+
+            int new_cost = cost + map[current_node][i];
+            int before_cost = dist[i];
 
             // if new_cost < before_cost, update
             if(new_cost < before_cost) {
-                dist[test_node] = new_cost;
-                pq.push( make_pair(-1 * new_cost,test_node) );
+                dist[i] = new_cost;
+                pq.push( make_pair(-1 * new_cost, i) );
             }
         }
     }
+
+    if(dist[to] == INF) return -1;
+    else return dist[to];
 }
 
 int main() {
     // put inputs & initialization
-    scanf("%d %d", &V, &E);
-    scanf("%d", &K);
+    scanf("%d %d", &N, &E);
     while(E--){
         int from, to, value ;
         scanf("%d %d %d", &from, &to, &value);
-        map[from].push_back(make_pair(to,value));
+        map[from][to] = value;
+        map[to][from] = value;
     }
-    for(int i = 1; i <= V; i++)
-        dist[i] = INF;
+    scanf("%d %d", &v1, &v2);
 
     // solution by Dijkstra Algorithm
-    Dijkstra();
+    int d1 = INF, d2 = INF;
+    if(Dijkstra(1,v1) != -1 && Dijkstra(v1,v2) != -1 && Dijkstra(v2,N) != -1)
+        d1 = Dijkstra(1, v1) + Dijkstra(v1, v2) + Dijkstra(v2, N);
+    if(Dijkstra(1,v2) != -1 && Dijkstra(v2,v1) != -1 && Dijkstra(v1,N) != -1)
+        d2 = Dijkstra(1, v2) + Dijkstra(v2, v1) + Dijkstra(v1, N);
 
-    // print
-    for(int i = 1; i <= V; i++){
-        if(dist[i] != INF) printf("%d\n", dist[i]);
-        else printf("INF\n");
-    }
-
+    if(d1 == INF && d2 == INF)
+        printf("-1\n");
+    else
+        printf("%d\n", min(d1, d2));
 }
