03edd62c6f686b502fde90a5acff86be1ed8b115
YeonSang <samshin3910@snu.ac.kr>
2020-08-17 03:16:22 +0900

    Baekjoon 11404

--- main.cpp
+++ main.cpp
@@ -1,69 +1,48 @@
 #include <cstdio>
-#include <vector>
+#include <algorithm>>
 #define INF 999999999
 using namespace std;
 
-typedef struct Road {
-    int from;
-    int to;
-    int value;
-};
-int N, M;
-bool isChanged = false;
-vector<Road> map;  // {from,to,value}
-long long int dist[502];  // dist[i] : minimum distance to node i, updated
+int n, m;
+int dp[102][102];  // initial: dp[from][to] = value
 
-void Relaxation() {
-    // relax EVERY ROADS
-    for(int i = 0; i < map.size(); i++){
-        int from_node = map[i].from;
-        int to_node = map[i].to;
-        int value = map[i].value;
-        // update
-        if(dist[from_node] == INF) continue;
-        if(dist[to_node] > dist[from_node] + value) {
-            dist[to_node] = dist[from_node] + value;
-            isChanged = true;
+void Floyd_Warshall() {
+    // consider passing node 'temp'
+    for(int temp = 1; temp <= n; temp++){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(dp[i][temp] != INF && dp[temp][j] != INF){
+                    dp[i][j] = min(dp[i][j], dp[i][temp] + dp[temp][j]);
+                }
+            }
         }
     }
 }
-
-void BellmanFord() {
-    // Bellman-Ford Algorithm
-    dist[1] = 0;    // initialization
-    // Relaxation: N-1 times
-    for(int i = 1; i <= N-1; i++){
-        // relax EVERY ROADS every time
-        Relaxation();
-    }
-    // check if there is negative-cycle
-    // if dist changes after N-1 times of Relaxation, it means that there is a negative-cycle
-    isChanged = false;
-    Relaxation();
-    if(isChanged) {
-        printf("-1\n");
-        return;
-    }
-    else {
-        for(int i = 2; i <= N; i++){
-            if(dist[i] == INF) printf("-1\n");
-            else printf("%d\n", dist[i]);
-        }
-    }
-}
-
 int main() {
-    // put inputs
-    scanf("%d %d", &N, &M);
-    while(M--){
-        int A, B, C;
-        scanf("%d %d %d", &A, &B, &C);
-        map.push_back( {A,B,C} );
+    // put inputs & initalization
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            dp[i][j] = INF;
+        }
+    }
+    while(m--){
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        if(dp[a][b] > c)
+            dp[a][b] = c;
     }
 
-    for(int i = 2; i <= N; i++){
-        dist[i] = INF;
+    // Solution by Floyd-Warshall Algorithm
+    Floyd_Warshall();
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(dp[i][j] == INF || i == j) printf("0 ");
+            else printf("%d ", dp[i][j]);
+        }
+        printf("\n");
     }
-    // Solution by Bellman-Ford Algorithm
-    BellmanFord();
 }
