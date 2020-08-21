da41abb8c7471e6ead28b06b06b56b338b14e547
YeonSang <samshin3910@snu.ac.kr>
2020-08-22 01:23:20 +0900

    Baekjoon 1991

--- main.cpp
+++ main.cpp
@@ -1,57 +1,70 @@
 #include <cstdio>
-#include <vector>
-#include <cstring>
-#define MAX_NODE 100000
+#define MAX_NODE 26
 using namespace std;
 
-// Idea : pick the farthest node from any node
-// Then, find the farthest node from 'that' node
+typedef struct Node {
+    int left;
+    int right;
+};
+int N;
+Node tree[MAX_NODE+2];
 
-int n, farthest_node, max_value;
-bool visit[MAX_NODE+2];     // check if visited or not
-vector< pair<int,int> > tree[MAX_NODE+2];   // tree[start].{end,value}
+// char to int
+int ctoi(char c) {
+    return c - 'A';
+}
+// int to char
+char itoc(int n) {
+    return n + 'A';
+}
 
-void dfs(int node, int dist){
-    // if already visited, return
-    if(visit[node])
-        return;
-
-    // update
-    visit[node] = true;
-    if(max_value < dist) {
-        max_value = dist;
-        farthest_node = node;
+void visit(int node){
+    if(node != ctoi('.')) {
+        printf("%c",itoc(node));
     }
+}
 
-    // dfs
-    for(int i = 0; i < tree[node].size(); i++) {
-        int next_node = tree[node].at(i).first;
-        int next_value = dist + tree[node].at(i).second;
-        // if not visited yet, 'node' is parent of 'next_node'
-        if(!visit[next_node]) {
-            dfs(next_node, next_value);
-        }
+void preorderTraversal(int name) {
+    if(name != ctoi('.')){
+        visit(name);
+        preorderTraversal(tree[name].left);
+        preorderTraversal(tree[name].right);
+    }
+}
+
+void inorderTraversal(int name){
+    if(name != ctoi('.')){
+        inorderTraversal(tree[name].left);
+        visit(name);
+        inorderTraversal(tree[name].right);
+    }
+}
+
+void postorderTraversal(int name){
+    if(name != ctoi('.')) {
+        postorderTraversal(tree[name].left);
+        postorderTraversal(tree[name].right);
+        visit(name);
     }
 }
 
 int main() {
     // save inputs
-    scanf("%d", &n);
-    for(int i = 0; i < n-1; i++){
-        int start, end, value;
-        scanf("%d %d %d", &start, &end, &value);
-        tree[start].push_back(make_pair(end,value));
-        tree[end].push_back(make_pair(start,value));
+    char buffer;
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++){
+        scanf("%c", &buffer);   // collect '\n'
+        char node, left, right;
+        scanf("%c %c %c", &node, &left, &right);
+        tree[ctoi(node)].left = ctoi(left);
+        tree[ctoi(node)].right = ctoi(right);
     }
 
-    // find the farthest node from any node. (This case, from node 1)
-    memset(visit, false, sizeof(visit));
-    dfs(1,0);
-
-    // find the max_distance from 'farthest_node'
-    memset(visit, false, sizeof(visit));
-    dfs(farthest_node, 0);
-
-    // print
-    printf("%d\n", max_value);
+    // print answers
+    preorderTraversal(ctoi('A'));
+    printf("\n");
+    inorderTraversal(ctoi('A'));
+    printf("\n");
+    postorderTraversal(ctoi('A'));
+    printf("\n");
 }
