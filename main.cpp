c6108d38196ea3481753c59f4bda0a68b6c76a6c
YeonSang <samshin3910@snu.ac.kr>
2020-08-23 23:06:11 +0900

    Baekjoon 4195

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include <cstdio>
-#define MAX_N 200
-#define MAX_M 1000
+#include <iostream>
+#include <map>
+#define MAX_N 100000
 using namespace std;
 
-int N, M;
-bool ans = true;
+int id = 1;
 int parent[MAX_N+3];    // parent[i] : i'th node's parent
-int level[MAX_N+3];     // level[i]: tree level which i'th node is included
-int plan[MAX_M+3];
+int size[MAX_N+3];      // size of tree[i]
+map<string,int> m;      // information about people {name,id}
+
 int find(int u) {
     // if root node, return
     if(u == parent[u])
@@ -30,56 +30,49 @@ void merge(int u, int v){
     if(u == v)
         return;
 
-    // if tree 'u' is deeper than 'v', swap
-    if(level[u] > level[v]){
+    // if tree 'u' is bigger than 'v', swap
+    if(size[u] > size[v]){
         int temp = u;
         u = v;
         v = temp;
     }
 
-    // merge two trees
+    // merge two trees ( u is added to v )
     parent[u] = v;
-
-    // if the two trees have same level, +1
-    if(level[u] == level[v])
-        level[v]++;
+    size[v] += size[u];
+    size[u] = 0;
 }
 
 int main() {
-    scanf("%d", &N);
-    scanf("%d", &M);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--){
+        // initialization
+        for(int i = 1; i <= MAX_N; i++){
+            parent[i] = i;  // root node : parent is itself
+            size[i] = 1;
+        }
 
-    // initialization
-    for(int i = 1; i <= N; i++){
-        parent[i] = i;  // root node : parent is itself
-        level[i] = 1;
-    }
+        int F;
+        cin >> F;
+        while(F--){
+            string s1, s2;
+            cin >> s1 >> s2;
+            // if new name, insert to map
+            if(m.count(s1) == 0)
+                m.insert(make_pair(s1,id++));
+            if(m.count(s2) == 0)
+                m.insert(make_pair(s2,id++));
 
-    // Merge by inputs
-    int input;
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= N; j++){
-            scanf("%d", &input);
-            if(input == 1){
-                merge(i,j);
-            }
+            // find root
+            int s1_root = find(m[s1]);
+            int s2_root = find(m[s2]);
+
+            // merge and print answer
+            merge(s1_root, s2_root);
+            cout << max(size[s1_root],size[s2_root]) << '\n';
         }
     }
-
-    // get plan
-    for(int i = 0; i < M; i++) {
-        scanf("%d", &plan[i]);
-    }
-
-    // find if we can travel by plan
-    for(int i = 0; i < M-1; i++){
-        if( find(plan[i]) != find(plan[i+1]) ) {
-            ans = false;
-            break;
-        }
-    }
-
-    // print answer
-    if(ans) printf("YES\n");
-    else printf("NO\n");
 }
