81179cd59c9e721ea8b6dd6fc077a95c55ca6946
YeonSang <samshin3910@snu.ac.kr>
2020-08-24 00:11:53 +0900

    Baekjoon 10775

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
-#include <map>
-#define MAX_N 100000
+#include <cstdio>
+#define MAX_G 100000
 using namespace std;
 
-int id = 1;
-int parent[MAX_N+3];    // parent[i] : i'th node's parent
-int size[MAX_N+3];      // size of tree[i]
-map<string,int> m;      // information about people {name,id}
-
+int G, P, main_root, ans;
+int parent[MAX_G+3];    // parent[i] : i'th node's parent
+int input[MAX_G+3];
 int find(int u) {
     // if root node, return
     if(u == parent[u])
@@ -30,49 +27,33 @@ void merge(int u, int v){
     if(u == v)
         return;
 
-    // if tree 'u' is bigger than 'v', swap
-    if(size[u] > size[v]){
-        int temp = u;
-        u = v;
-        v = temp;
-    }
-
-    // merge two trees ( u is added to v )
+    // merge two trees
     parent[u] = v;
-    size[v] += size[u];
-    size[u] = 0;
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int T;
-    cin >> T;
-    while(T--){
-        // initialization
-        for(int i = 1; i <= MAX_N; i++){
-            parent[i] = i;  // root node : parent is itself
-            size[i] = 1;
-        }
-
-        int F;
-        cin >> F;
-        while(F--){
-            string s1, s2;
-            cin >> s1 >> s2;
-            // if new name, insert to map
-            if(m.count(s1) == 0)
-                m.insert(make_pair(s1,id++));
-            if(m.count(s2) == 0)
-                m.insert(make_pair(s2,id++));
-
-            // find root
-            int s1_root = find(m[s1]);
-            int s2_root = find(m[s2]);
-
-            // merge and print answer
-            merge(s1_root, s2_root);
-            cout << max(size[s1_root],size[s2_root]) << '\n';
-        }
+    scanf("%d", &G);
+    scanf("%d", &P);
+    // initialization
+    for(int i = 0; i <= G; i++){
+        parent[i] = i;  // root node : parent is itself
     }
+
+    // put inputs
+    for(int i = 1; i <= P; i++){
+        scanf("%d", &input[i]);
+    }
+
+    // solve question
+    for(ans = 1; ans <= P; ans++){
+        int temp = find(input[ans]);
+        // if the result of "find(input)" is 0, it means that there is no space left
+        if(temp != 0){
+            // union the gate and the gate before
+            merge(temp, temp-1);
+            continue;
+        }
+        else break;
+    }
+    printf("%d\n", ans-1);
 }
